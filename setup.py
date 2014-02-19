27782ea1f81d3e8002659190e9e101eeb26b2b14
Alex Lovell-Troy <alex@lovelltroy.org>
2014-02-19 10:31:53 -0600

    getting the setuptools and MANIFEST right

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
 from distutils.core import setup
+import os
+
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('telemetry'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+print data_files
 
 setup(
     name='django-telemetry',
@@ -14,6 +36,12 @@ setup(
         'telemetry.reporting',
         'telemetry.requests',
     ],
+    include_package_data=True,
+    package_data = { 'telemetry.reporting': [
+        "static/javascripts/*.js",
+        "static/*.css",
+        "templates/*.html",
+    ]},
     url='https://github.com/alexlovelltroy/django-telemetry',
     license='LICENSE.txt',
     long_description=open('README.md').read(),
